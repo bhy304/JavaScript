4d5cb9fb2a61959d748d0002716c952f95edb701
bhy304 <bhy0512@gmail.com>
2019-08-14 23:09:22 +0900

    가위바위보 게임 업데이트

--- WebGame/rockPaperScissors.js
+++ WebGame/rockPaperScissors.js
@@ -5,27 +5,52 @@ var RPS = { //딕셔너리 자료구조
     paper: '-284px' 
 };
 
-//console.log(Object.entries(RPS)); //Object to Array
 function chooseByComp(bgPosition) {
+    //  Object.entries(RPS) : Object to Array
     return Object.entries(RPS).find(function(value){
         return value[1] === bgPosition;
     })[0];
 }
 
-setInterval(function() {
-    if (bgPosition === RPS.rock) { //0
-        bgPosition = RPS.scissor;
-    } else if (bgPosition === RPS.scissor) { //-135
-        bgPosition = RPS.paper;
-    } else {
-        bgPosition = RPS.rock;
-    }
-    document.querySelector("#computer").style.background = 'url(http://en.pimg.jp/023/182/267/1/23182267.jpg) ' + bgPosition + ' 0';
-}, 100);
+var interval;
+function intervalMaker() {
+    interval = setInterval(function() {
+        if (bgPosition === RPS.rock) { 
+            bgPosition = RPS.scissor;
+        } else if (bgPosition === RPS.scissor) { 
+            bgPosition = RPS.paper;
+        } else {
+            bgPosition = RPS.rock;
+        }
+        document.querySelector("#computer").style.background = 'url(http://en.pimg.jp/023/182/267/1/23182267.jpg) ' + bgPosition + ' 0';
+    }, 100);
+}
+intervalMaker();
+
+var point = {
+    scissor: 1,
+    rock: 0,
+    paper: -1
+};
 
 document.querySelectorAll(".jsBtn").forEach(function(btn) {
     btn.addEventListener("click", function() {
+        clearInterval(interval); // clearInterval로 setInterval을 멈춘다.
+        setTimeout(function() {
+            intervalMaker();
+        }, 1000);
+
         let chooseByMe = this.textContent;
-        console.log(chooseByMe, chooseByComp(bgPosition));
+        var myPoint = point[chooseByMe];
+        var compPoint = point[chooseByComp(bgPosition)];
+        var result = myPoint - compPoint;
+
+        if(result === 0) {
+            console.log('Tie...');
+        } else if ([-1, 2].includes(result)) {
+            console.log('Win!!!');
+        } else {
+            console.log('Lose ㅠㅠ');
+        }
     });
 });
